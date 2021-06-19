ebd88ecebd679f2ac3153beb89a06b58faf0be23
SW-H <hsw08@naver.com>
2021-06-19 23:27:25 +0900

    #1 feat : modify methods for oop programming add getter/setter, add methods for each fields, modify isValid()

--- src/calender/MyDate.java
+++ src/calender/MyDate.java
@@ -4,35 +4,68 @@ public class MyDate {
 	private int date;
 	private int month;
 	private int year;
+
+	public boolean isValid;
 	int[] dateRange = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+	public MyDate() {
+		isValid = true;
+	}
+
 	public MyDate(int date, int month, int year) {
-		this.date = date;
-		this.month = month;
+		isValid = true;
+		setYear(year);
+		setMonth(month);
+		setDate(date);
+	}
+
+	public int getDate() {
+		return date;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
 		this.year = year;
 	}
 
-	public boolean isLeapYear(int year) {
+	public void setMonth(int month) {
+		if (1 <= month && month <= 12)
+			this.month = month;
+		else
+			isValid = false;
+	}
+
+	public void setDate(int date) {
+		setDateRange(year);
+		if (date <= dateRange[month])
+			this.date = date;
+		else
+			isValid = false;
+	}
+
+	public void setDateRange(int year) {
 		/*
-		 * 윤년 계산법
-		 * 1. 4로 나누어 떨어진다 = 윤년
-		 * 		단, 100으로도 나누어 떨어진다 = 평년
+		 * 윤년 계산법 
+		 * 1. 4로 나누어 떨어진다 = 윤년 
+		 * 		단, 100으로도 나누어 떨어진다 = 평년 
 		 * 2. 4로 나누어 떨어지고, 100으로도 나누어 떨어지나, 400으로 나누어 떨어진다 = 윤년
-		 *  
 		 */
 		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-			return true;
-		return false;
+			dateRange[2]++;
 	}
 
 	public String isValid() {
-		if (!(1 <= date && date <= 31) || !(1 <= month && month <= 12))
+		if (isValid == true)
+			return "유효한 날짜입니다";
+		else
 			return "유효하지 않은 날짜입니다";
-		if ((month == 2) && (isLeapYear(year) == true))
-			dateRange[month]++;
-		if (date > dateRange[month])
-			return "유효하지 않은 날짜입니다";
-		return "유효한 날짜입니다";
 
 	}
 }
